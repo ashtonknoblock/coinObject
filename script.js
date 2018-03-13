96571624f19b24ea76c75e740a869c0145545bff
Ashton Knoblock <Ashton.knoblock@gmail.com>
2018-03-13 13:07:54 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+var coin = {
+    state: 0,
+    flip: function() {
+        // randomly set this.state to be either 0 or 1
+         this.state = (Math.round(Math.random()));
+         return this;
+    },
+    toString: function() {
+        // return “H” or “T” depending on whether this.state is 0 or 1
+        if (this.state === 0){
+            return "H";
+        } else {
+            return "T";
+        }
+    },
+    toHTML: function() {
+        var img = document.createElement("img");
+        // set the properties of the image element to show either heads or tails
+        if (this.toString() === "H") { //heads picture
+            img.classList.add('coin', 'heads')
+        }
+        else if (this.toString() === "T") { //tails picture
+            img.classList.add('coin', 'tails')
+        }
+        return img;
+    }
+};
+
+var div = document.getElementById("test");
+var div2 = document.getElementById("test2");
+
+for (let i = 0; i < 20; i++){
+    var text = document.createTextNode(coin.flip().toString());
+    div2.appendChild(text);
+}
+
+for (let j = 0; j < 20; j++) {
+    div.appendChild(coin.flip().toHTML());
+}
+
